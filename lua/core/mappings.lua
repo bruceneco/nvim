8f0f2e80fe1d5c4ae226813aae57494c1a91a65f
Bruce Neco <bruce.neco@transfeera.com>
2025-03-31 11:41:06 -0300

    fix: smear-cursor and remap find file to double leader

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -270,7 +270,8 @@ M.telescope = {
 
   n = {
     -- find
-    ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+    -- ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+    ["<leader><leader>"] = {"<cmd> Telescope find_files <CR>", "Find files"},
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
     ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
     ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -7,13 +7,12 @@ local plugins = {
       scroll_buffer_space = true,
       legacy_computing_symbols_support = false,
       smear_insert_mode = true,
+      stiffness = 0.8,
+      trailing_stiffness = 0.3,
+      distance_stop_animating = 0.5,
     },
-    config = function()
-      require("smear_cursor").setup({
-        stiffness = 0.8,
-        trailing_stiffness = 0.4,
-        distance_stop_animating = 0.5,
-      })
+    config = function(_, opts)
+      require("smear_cursor").setup(opts)
     end
   },
   {

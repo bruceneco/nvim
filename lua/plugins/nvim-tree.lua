e6fedc30e9947e140172f99b59a42478f307b3b1
Bruce Neco <bruce.neco@transfeera.com>
2025-04-07 17:27:12 -0300

    feat: make nvim-tree float

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,15 +1,41 @@
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.3
+
 return {
   "nvim-tree/nvim-tree.lua",
-  event = "VeryLazy",
+  lazy = false,
+  priority = 1000,
   opts = {
     sync_root_with_cwd = true,
     respect_buf_cwd = true,
-    update_focused_file = {
-      enable = true,
-      update_root = true,
+    view = {
+      side = "right",
+      float = {
+        enable = true,
+        open_win_config = function()
+          local screen_w = vim.opt.columns:get()
+          local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+          local window_w = screen_w * WIDTH_RATIO
+          local window_h = screen_h * HEIGHT_RATIO
+          local window_w_int = math.floor(window_w)
+          local window_h_int = math.floor(window_h)
+          local center_x = (screen_w - window_w) / 2
+          local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+          return {
+            border = "rounded",
+            relative = "editor",
+            row = center_y,
+            col = center_x,
+            width = window_w_int,
+            height = window_h_int,
+          }
+        end,
+      },
+      width = function()
+        return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+      end,
     },
     renderer = {
-      full_name = true,
       icons = {
         git_placement = "signcolumn",
         show = {
@@ -21,7 +47,4 @@ return {
       },
     },
   },
-  setup = function(_, opts)
-    require("nvim-tree").setup(opts)
-  end,
 }

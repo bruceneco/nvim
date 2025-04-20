429d81be395aa5415b2a3bddd60aacc3a2ffce8f
Bruce Neco <bruce.neco@gmail.com>
2025-04-19 21:29:26 -0300

    chore: remove unused number tab switch

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,12 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
--- Alt+ num switch tabs
-for i = 1, 9, 1 do
-  map("n", string.format("<A-%s>", i), function()
-    vim.api.nvim_set_current_buf(vim.t.bufs[i])
-  end)
-end
 
 map("n", "]d", function()
   vim.diagnostic.goto_next { float = true }

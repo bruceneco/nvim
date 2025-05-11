7eb581b16bbcec24b00140bf8fd781ec9f58f2b1
Bruce Neco <bruce.neco@gmail.com>
2025-05-11 09:41:31 -0300

    feat: add new coloschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,39 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      palette = "darker",
+    },
+    setup = function(_, opts)
+      require("eldritch").load(opts)
+    end,
   },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    opts = {
+      transparent = true,
+    },
+  },
+  { "oxfist/night-owl.nvim", lazy = true, opts = { transparent_background = true } },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "cyberdream",
     },
   },
   {
     "catppuccin/nvim",
-    lazy = true,
+    lazy = false,
     name = "catppuccin",
     opts = {
       flavour = "mocha",

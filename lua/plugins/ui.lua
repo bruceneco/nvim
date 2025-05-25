301aa7fda10575dad4248294a3e9c4d96553c272
Bruce Neco <bruce.neco@gmail.com>
2025-05-24 22:22:29 -0300

    feat: add outline, copilot chat and neotest windows

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -63,10 +63,12 @@ return {
         },
         left = {
           { title = "Filesystem", ft = "neo-tree", pinned = true },
-          { title = "Neotest Summary", ft = "neotest-summary" },
+          { title = "Neotest Summary", ft = "neotest-summary", pinned = true },
         },
         right = {
           { title = "Grug Far", ft = "grug-far", size = { width = 0.4 } },
+          { title = "Outline", ft = "Outline", size = { width = 0.25 }, open = "Outline", pinned = true },
+          { title = "Copilot", ft = "copilot-chat", size = { width = 0.4 }, pinned = true, open = "CopilotChat" },
         },
         keys = {
           -- increase width

8fb70a4edb2e012657e50391a1ca0e565fda23b5
Bruce Neco <bruce.neco@gmail.com>
2025-05-25 12:47:44 -0300

    feat: store and restart tasks from overseer on session

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,6 +1,10 @@
 local function close()
   vim.cmd("windo if &buftype != '' | close | endif")
 end
+local function get_cwd_as_name()
+  local dir = vim.fn.getcwd(0)
+  return dir:gsub("[^A-Za-z0-9]", "_")
+end
 return {
   {
     "rmagatti/auto-session",
@@ -17,6 +21,30 @@ return {
           load_on_setup = false,
           previewer = false,
           theme_conf = { border = true },
+        },
+        pre_save_cmds = {
+          function()
+            require("overseer").save_task_bundle(
+              get_cwd_as_name(),
+              -- Passing nil will use config.opts.save_task_opts. You can call list_tasks() explicitly and
+              -- pass in the results if you want to save specific tasks.
+              nil,
+              { on_conflict = "overwrite" } -- Overwrite existing bundle, if any
+            )
+          end,
+        },
+        -- Optionally get rid of all previous tasks when restoring a session
+        pre_restore_cmds = {
+          function()
+            for _, task in ipairs(require("overseer").list_tasks({})) do
+              task:dispose(true)
+            end
+          end,
+        },
+        post_restore_cmds = {
+          function()
+            require("overseer").load_task_bundle(get_cwd_as_name(), { ignore_missing = true })
+          end,
         },
       })
     end,

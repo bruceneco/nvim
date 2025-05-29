09287d6f1ff1a469675df4a227a8f36b00edcbc2
Bruce Neco <bruce.neco@gmail.com>
2025-05-29 20:20:45 -0300

    feat: add dap adpater for jest

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -1,5 +1,46 @@
 return {
   {
+    "mxsdev/nvim-dap-vscode-js",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      require("dap-vscode-js").setup({
+        adapters = { "pwa-node" },
+        debugger_cmd = { "js-debug-adapter" },
+      })
+      require("dap").adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "js-debug-adapter",
+          args = { "${port}" },
+        },
+      }
+
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        require("dap").configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Jest Tests",
+            -- trace = true, -- include debugger info
+            runtimeExecutable = "node",
+            runtimeArgs = {
+              "./node_modules/jest/bin/jest.js",
+              "--runInBand",
+            },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+          },
+        }
+      end
+    end,
+  },
+  {
     "nvim-neotest/neotest",
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {

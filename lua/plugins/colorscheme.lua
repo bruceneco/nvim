dcf8ecd967e79d73651fb47c52722251c96192d6
Bruce Neco <bruce.neco@gmail.com>
2025-08-23 22:37:33 -0300

    chore: remove unused themes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,39 +1,10 @@
 return {
   {
-    "eldritch-theme/eldritch.nvim",
-    lazy = true,
-    priority = 1000,
-    opts = {
-      transparent = true,
-      palette = "darker",
-    },
-    setup = function(_, opts)
-      require("eldritch").load(opts)
-    end,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    lazy = true,
-    opts = {},
-  },
-  {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     opts = {
       transparent = true,
     },
-  },
-  {
-    "oxfist/night-owl.nvim",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      require("night-owl").setup({
-        transparent_background = false,
-      })
-      vim.cmd.colorscheme("night-owl")
-    end,
   },
   {
     "LazyVim/LazyVim",
@@ -49,15 +20,6 @@ return {
         sidebars = "transparent",
         floats = "transparent",
       },
-    },
-  },
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-      transparent_background = true,
     },
   },
 }

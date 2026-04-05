332b2746fb557e9bfe8f0c449ae47cbee5cf1a36
Bruce Neco <bruce.neco@gmail.com>
2026-04-05 15:09:54 -0300

    chore: move config from setup to opts

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,63 +1,29 @@
 local function close()
   vim.cmd("windo if &buftype != '' | close | endif")
 end
--- local function get_cwd_as_name()
---   local dir = vim.fn.getcwd(0)
---   return dir:gsub("[^A-Za-z0-9]", "_")
--- end
 return {
   {
     "rmagatti/auto-session",
     lazy = false,
-    config = function()
-      require("auto-session").setup({
-        suppressed_dirs = { "~/", "~/Projects", "~/projects", "~/Downloads", "/" },
-        auto_restore = true,
-        cwd_change_handling = false,
-        auto_save = false,
-        auto_delete_empty_sessions = false,
-        continue_restore_on_error = true,
-        log_level = nil,
-        session_lens = {
-          picker = "snacks",
-          picker_opts = {
-            previewer = false,
-          },
-          load_on_setup = false,
-          previewer = false,
-          theme_conf = { border = true },
-        },
-        -- pre_save_cmds = {
-        --   function()
-        --     require("overseer").save_task_bundle(
-        --       get_cwd_as_name(),
-        --       -- Passing nil will use config.opts.save_task_opts. You can call list_tasks() explicitly and
-        --       -- pass in the results if you want to save specific tasks.
-        --       nil,
-        --       { on_conflict = "overwrite" } -- Overwrite existing bundle, if any
-        --     )
-        --   end,
-        -- },
-        -- Optionally get rid of all previous tasks when restoring a session
-        -- pre_restore_cmds = {
-        --   function()
-        --     for _, task in ipairs(require("overseer").list_tasks({})) do
-        --       task:dispose(true)
-        --     end
-        --   end,
-        -- },
-        -- post_restore_cmds = {
-        --   function()
-        --     require("overseer").load_task_bundle(get_cwd_as_name(), { ignore_missing = true })
-        --   end,
-        -- },
-      })
-    end,
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/projects", "~/Downloads", "/" },
+      auto_restore = false,
+      cwd_change_handling = false,
+      auto_save = false,
+      auto_delete_empty_sessions = false,
+      continue_restore_on_error = true,
+      log_level = nil,
+      git_use_branch_name = true,
+      session_lens = {
+        enabled = false,
+      },
+    },
     keys = {
       {
         "<leader>qq",
         function()
-          -- close()
           vim.cmd("SessionSave")
           vim.cmd("qa")
         end,
@@ -67,7 +33,6 @@ return {
       {
         "<leader>qs",
         function()
-          -- close()
           vim.cmd("SessionSave")
           vim.cmd("SessionSearch")
         end,
